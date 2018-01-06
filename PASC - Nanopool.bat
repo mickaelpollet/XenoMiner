5e7c000c2f1f5b01dc92e6ab94115b960fad7d0a
Mickaël POLLET <mickaelpollet@gmail.com>
2018-01-06 07:49:48 +0100

    - Added the 60% fan speed by default with ETH mining

--- PASC - Nanopool.bat
+++ PASC - Nanopool.bat
@@ -4,4 +4,4 @@ setx GPU_USE_SYNC_OBJECTS 1
 setx GPU_MAX_ALLOC_PERCENT 100
 setx GPU_SINGLE_ALLOC_PERCENT 100
 cd claymore
-EthDcrMiner64.exe -epool pasc-eu1.nanopool.org:15555 -ewal 86646.63e697d558ed82e6.Dobby2 -epsw x -mode 1 -ethi 11
+EthDcrMiner64.exe -epool pasc-eu1.nanopool.org:15555 -ewal 86646.63e697d558ed82e6.Dobby2 -epsw x -mode 1 -ethi 11 -tt -60
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # XenoMiner
 Mining library
+
+v1.0.1
+- Added the 60% fan speed by default with ETH mining
+
+v1.0.0
+- Initial commit.
+- Claymore Dual Miner for Windows works fine
+- XmrMiner fine too
